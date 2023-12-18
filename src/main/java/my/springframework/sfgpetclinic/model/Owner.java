477e161fdc6bb8c6bb15a13d5b1e249e40a31981
Marko Markovic <markomarkoviccb@gmail.com>
2023-12-18 14:34:36 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/my/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package my.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/my/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package my.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
