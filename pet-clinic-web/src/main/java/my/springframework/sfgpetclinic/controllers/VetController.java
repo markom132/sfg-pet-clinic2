350eacffe022e6101d2153d12ff5414c0736c8e5
Marko Markovic <markomarkoviccb@gmail.com>
2023-12-25 14:32:48 +0100

    66. adding vets page and controller. closes#7

---
+++ pet-clinic-web/src/main/java/my/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package my.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets.html", "vets/index", "vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
