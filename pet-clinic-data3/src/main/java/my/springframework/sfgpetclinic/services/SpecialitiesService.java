648827cd6847f1147c1021874a7748faed4e1a5f
Marko Markovic <markomarkoviccb@gmail.com>
2024-04-08 15:42:23 +0200

    124. Spring Pet Clinic - Create Speciality Map Service Closes #32

---
+++ pet-clinic-data3/src/main/java/my/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package my.springframework.sfgpetclinic.services;
+
+import my.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+}
