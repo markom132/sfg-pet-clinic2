5504266006aa720aa6955399d936149b2f17b249
Marko Markovic <markomarkoviccb@gmail.com>
2024-05-15 13:34:08 +0200

    148. Spring Pet Clinic - Create Visit JPA Entities Closes #39

--- pet-clinic-data3/src/main/java/my/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data3/src/main/java/my/springframework/sfgpetclinic/model/Vet.java
@@ -6,11 +6,10 @@ import java.util.Set;
 
 @Entity
 @Table(name = "vets")
-public class Vet extends Person{
+public class Vet extends Person {
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
-            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
